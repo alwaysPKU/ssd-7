8897f1f662ba0b06ab9ee855e74fa59876dcb442
alwaysqi <zw_pku@163.com>
2018-11-26 20:44:16 +0800

    tf_convert

--- sh/tf_convert.sh
+++ sh/tf_convert.sh
@@ -1,3 +1,3 @@
 DATASET_DIR=/home/pengkun/zhangwei/SSD-Tensorflow-7/datasets_voc/2007/VOCdevkit/VOC2007-7-new/
 OUTPUT_DIR=/home/pengkun/zhangwei/SSD-Tensorflow-7/datasets_tf_7
-python ../tf_convert_data.py --dataset_name=pascalvoc --dataset_dir=${DATASET_DIR} --output_name=voc_2007_train --output_dir=${OUTPUT_DIR}
+python ../tf_convert_data.py --dataset_name=pascalvoc --dataset_dir=${DATASET_DIR} --output_name=voc_2007_test --output_dir=${OUTPUT_DIR}
